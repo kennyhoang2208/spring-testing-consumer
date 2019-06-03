99f1c9bfbdcfae994e6e5bc762ec9539669be3a7
Kenny Hoang <kennyhoang2208@gmail.com>
2019-06-03 13:39:01 +0700

    Add missing tag

--- deploy_dependency_services.sh
+++ deploy_dependency_services.sh
@@ -22,6 +22,7 @@ for service in "${SERVICES[@]}"; do
 
   # Build and deploy in to the preview environment
   cd ${PROJ_PARENT}/${service}
+  sh "echo $(jx-release-version) > VERSION"
   gradle clean build
   export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml
   jx step post build --image ${DOCKER_REGISTRY}/${ORG}/${service}:$(cat VERSION)
