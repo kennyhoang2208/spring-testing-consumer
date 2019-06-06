e1ccd9621868f3bfd35d5e4548e29497c970ca6a
Kenny Hoang <kennyhoang2208@gmail.com>
2019-06-06 17:01:17 +0700

    Add spring cloud kubernetes for discorery services

--- src/main/java/example/ExampleConsumerApplication.java
+++ src/main/java/example/ExampleConsumerApplication.java
@@ -2,10 +2,12 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "example")
+@EnableDiscoveryClient
 public class ExampleConsumerApplication {
 
     public static void main(String[] args) {
--- src/main/java/example/WeatherController.java
+++ src/main/java/example/WeatherController.java
@@ -3,6 +3,7 @@ package example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,9 @@ public class WeatherController {
     private static final Logger logger = LoggerFactory.getLogger(WeatherController.class);
 
     private WeatherServiceClient weatherServiceClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     public WeatherController(WeatherServiceClient weatherServiceClient) {
