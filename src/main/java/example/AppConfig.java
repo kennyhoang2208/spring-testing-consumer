cfdc153f97eb167c800bd4c07d385d2fe7c74417
Kenny Hoang <kennyhoang2208@gmail.com>
2019-06-10 16:05:41 +0700

    Add proxy

--- src/main/java/example/AppConfig.java
+++ src/main/java/example/AppConfig.java
@@ -4,13 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @Configuration
 public class AppConfig {
     @Bean
     RestTemplate restTemplate() {
-        return new RestTemplate();
+
+        SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("35.245.56.57", 80));
+        clientHttpRequestFactory.setProxy(proxy);
+
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
     @Bean
