e3c31ae80d7d7f02de4f16e218b21d90bcdc8400
Kenny Hoang <kennyhoang2208@gmail.com>
2019-06-10 15:35:46 +0700

    Add spring cloud dependencies

--- src/main/java/example/ExampleConsumerApplication.java
+++ src/main/java/example/ExampleConsumerApplication.java
@@ -2,10 +2,12 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 @ComponentScan(basePackages = "example")
 public class ExampleConsumerApplication {
