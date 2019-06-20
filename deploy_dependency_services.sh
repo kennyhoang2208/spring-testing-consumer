4023f32cff22f872ea0213b3b110e36e733bb91e
Kenny Hoang <kennyhoang2208@gmail.com>
2019-06-20 15:45:21 +0700

    Reduce build image step

--- deploy_dependency_services.sh
+++ deploy_dependency_services.sh
@@ -26,10 +26,13 @@ for service in "${DEPENDENCY_SERVICES[@]}"; do
 
   # Build and deploy in to the preview environment
   cd ${PROJ_PARENT}/${service}
-  echo $(jx-release-version) > VERSION
-  gradle clean build
+
+  # How about using the latest version ???
+  echo $(jx-release-version -same-release) > VERSION
+
+  # gradle clean build
   export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml
-  jx step post build --image ${DOCKER_REGISTRY}/${ORG}/${service}:$(cat VERSION)
+  # jx step post build --image ${DOCKER_REGISTRY}/${ORG}/${service}:$(cat VERSION)
 
   # Config for service preview naming.
   export PREVIEW_VERSION=$VERSION
